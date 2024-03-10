90ee7b5affde344c74b7d1159ee1622ca01eff9b
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2024-03-10 14:54:39 +0100

    utils file is updated with load_pickle file

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,18 @@
+import sys
+import os
 import yaml
+import joblib
+
+sys.path.append("src/")
 
 
 def params():
     with open("./default_params.yml", "r") as file:
         return yaml.safe_load(file)
+
+
+def load_pickle(path):
+    if os.path.exists(path):
+        return joblib.load(filename=path)
+    else:
+        raise Exception("Path is not found".capitalize())
---
+++ unittest/unittest.py
@@ -0,0 +1,9 @@
+import unittest
+
+
+class UnitTest(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_quantity_of_data(self):
+        pass
